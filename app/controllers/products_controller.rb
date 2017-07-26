b10eca4f1058626db724fa18d0186e0b29440506
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-07-26 11:51:03 -0700

    add devise views, if_admin? function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  before_action :only => [:new, :edit] do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
+
   # GET /products
   # GET /products.json
   def index
